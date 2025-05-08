e50673e92de6d55ae0689f38840058e202ef6690
PopaBogdan498 <bogdan-constantin.popa1@s.unibuc.ro>
2025-05-08 13:12:01 +0300

    Add files via upload
    BTree cu tot cu header file

--- sd_2.cpp
+++ sd_2.cpp
@@ -1,68 +1,78 @@
-///INSTRUCTIUNI
-///1 pentru introducerea unui numar in binary tree
-///2 pentru cautarea unui numar( cout 1 for found, 0 for not found)
-///3 pentru scoaterea unui numar din binary tree
-///pentru input, un fisier "input.txt" cu un string de forma (nr comanda, nr asupra caruia se efectueaza comanda) * n
+/// INSTRUCTIUNI
+/// 1 pentru introducerea unui numar in binary tree
+/// 2 pentru cautarea unui numar (cout 1 for found, 0 for not found)
+/// 3 pentru scoaterea unui numar din binary tree
+/// pentru input, un fisier "input.txt" cu un string de forma (nr comanda, nr asupra caruia se efectueaza comanda) * n
 #include <iostream>
 #include <fstream>
+#include "BinaryTree.h"
 using namespace std;
 
-struct Node {
-    int value;
-    Node* left;
-    Node* right;
-    Node(int val) : value(val), left(nullptr), right(nullptr) {}
-};
-
-Node* insert(Node* root, int val) {
-    if (!root) return new Node(val);
-    if (val < (*root).value)
-        (*root).left = insert((*root).left, val);
-    else if (val > (*root).value)
-        (*root).right = insert((*root).right, val);
-    return root;
+BTreeNode* BinaryTree::insert(BTreeNode* node, int val) {
+    if (!node) return new BTreeNode(val);
+    if (val < (*node).value)
+        (*node).left = insert((*node).left, val);
+    else if (val > (*node).value)
+        (*node).right = insert((*node).right, val);
+    return node;
 }
 
-bool search(Node* root, int val) {
-    if (!root) return false;
-    if (val == (*root).value) return true;
-    return val < (*root).value ? search((*root).left, val) : search((*root).right, val);
+bool BinaryTree::search(BTreeNode* node, int val) {
+    if (!node) return false;
+    if (val == (*node).value) return true;
+    return val < (*node).value ? search((*node).left, val) : search((*node).right, val);
 }
 
-Node* findMin(Node* root) {
-    while (root && (*root).left)
-        root = (*root).left;
-    return root;
+BTreeNode* BinaryTree::findMin(BTreeNode* node) {
+    while (node && (*node).left)
+        node = (*node).left;
+    return node;
 }
 
-Node* deleteNode(Node* root, int val) {
-    if (!root) return nullptr;
-    if (val < (*root).value)
-        (*root).left = deleteNode((*root).left, val);
-    else if (val > (*root).value)
-        (*root).right = deleteNode((*root).right, val);
+BTreeNode* BinaryTree::deleteNode(BTreeNode* node, int val) {
+    if (!node) return nullptr;
+    if (val < (*node).value)
+        (*node).left = deleteNode((*node).left, val);
+    else if (val > (*node).value)
+        (*node).right = deleteNode((*node).right, val);
     else {
-        if (!(*root).left) {
-            Node* temp = (*root).right;
-            delete root;
+        if (!(*node).left) {
+            BTreeNode* temp = (*node).right;
+            delete node;
             return temp;
-        } else if (!(*root).right) {
-            Node* temp = (*root).left;
-            delete root;
+        } else if (!(*node).right) {
+            BTreeNode* temp = (*node).left;
+            delete node;
             return temp;
         }
-        Node* temp = findMin((*root).right);
-        (*root).value = (*temp).value;
-        (*root).right = deleteNode((*root).right, (*temp).value);
+        BTreeNode* temp = findMin((*node).right);
+        (*node).value = (*temp).value;
+        (*node).right = deleteNode((*node).right, (*temp).value);
     }
-    return root;
+    return node;
 }
 
-void inorder(Node* root) {
-    if (!root) return;
-    inorder((*root).left);
-    cout << (*root).value << " ";
-    inorder((*root).right);
+void BinaryTree::inorder(BTreeNode* node) {
+    if (!node) return;
+    inorder((*node).left);
+    cout << (*node).value << " ";
+    inorder((*node).right);
+}
+
+void BinaryTree::insert(int val) {
+    root = insert(root, val);
+}
+
+bool BinaryTree::search(int val) {
+    return search(root, val);
+}
+
+void BinaryTree::deleteValue(int val) {
+    root = deleteNode(root, val);
+}
+
+void BinaryTree::inorderPrint() {
+    inorder(root);
 }
 
 int main() {
@@ -72,20 +82,20 @@ int main() {
         return 1;
     }
 
-    Node* root = nullptr;
+    BinaryTree tree;
     int a, b;
     while (fin >> a >> b) {
         if (a == 1) {
-            root = insert(root, b);
+            tree.insert(b);
         } else if (a == 2) {
-            cout << (search(root, b) ? 1 : 0) << " ";
+            cout << (tree.search(b) ? 1 : 0) << " ";
         } else if (a == 3) {
-            root = deleteNode(root, b);
+            tree.deleteValue(b);
         }
     }
 
     cout << "\n";
-    inorder(root);
+    tree.inorderPrint();
     cout << "\n";
 
     return 0;
