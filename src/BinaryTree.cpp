d684984b340545616b1f513e137505f9d07e2a3c
PopaBogdan498 <bogdan-constantin.popa1@s.unibuc.ro>
2025-05-21 10:29:58 +0300

    Btree RBtree

--- src/BinaryTree.cpp
+++ src/BinaryTree.cpp
@@ -5,6 +5,7 @@
 /// pentru input, un fisier "input.txt" cu un string de forma (nr comanda, nr asupra caruia se efectueaza comanda) * n
 #include <iostream>
 #include <fstream>
+#include <stack>
 #include "BinaryTree.h"
 using namespace std;
 
@@ -29,48 +30,130 @@ BTreeNode* BinaryTree::findMin(BTreeNode* node) {
     return node;
 }
 
-BTreeNode* BinaryTree::deleteNode(BTreeNode* node, int val) {
-    if (!node) return nullptr;
-    if (val < (*node).value)
-        (*node).left = deleteNode((*node).left, val);
-    else if (val > (*node).value)
-        (*node).right = deleteNode((*node).right, val);
-    else {
-        if (!(*node).left) {
-            BTreeNode* temp = (*node).right;
-            delete node;
-            return temp;
-        } else if (!(*node).right) {
-            BTreeNode* temp = (*node).left;
-            delete node;
-            return temp;
-        }
-        BTreeNode* temp = findMin((*node).right);
-        (*node).value = (*temp).value;
-        (*node).right = deleteNode((*node).right, (*temp).value);
-    }
-    return node;
-}
-
-void BinaryTree::inorder(BTreeNode* node) {
-    if (!node) return;
-    inorder((*node).left);
-    cout << (*node).value << " ";
-    inorder((*node).right);
-}
-
 void BinaryTree::insert(int val) {
-    root = insert(root, val);
+    BTreeNode* newNode = new BTreeNode(val);
+    if (!root) {
+        root = newNode;
+        return;
+    }
+
+    BTreeNode* curr = root;
+    BTreeNode* parent = nullptr;
+    while (curr) {
+        parent = curr;
+        if (val < curr->value)
+            curr = curr->left;
+        else if (val > curr->value)
+            curr = curr->right;
+        else {
+            delete newNode;
+            return;
+        }
+    }
+
+    if (val < parent->value)
+        parent->left = newNode;
+    else
+        parent->right = newNode;
 }
+
 
 bool BinaryTree::search(int val) {
-    return search(root, val);
+    BTreeNode* curr = root;
+    while (curr) {
+        if (val == curr->value)
+            return true;
+        else if (val < curr->value)
+            curr = curr->left;
+        else
+            curr = curr->right;
+    }
+    return false;
 }
 
-void BinaryTree::erase(int val) {
-    root = deleteNode(root, val);
+void BinaryTree::deleteValue(int val) {
+    BTreeNode* parent = nullptr;
+    BTreeNode* curr = root;
+
+    while (curr && curr->value != val) {
+        parent = curr;
+        if (val < curr->value)
+            curr = curr->left;
+        else
+            curr = curr->right;
+    }
+
+    if (!curr) return;
+
+    if (curr->left && curr->right) {
+        BTreeNode* successorParent = curr;
+        BTreeNode* successor = curr->right;
+        while (successor->left) {
+            successorParent = successor;
+            successor = successor->left;
+        }
+
+        curr->value = successor->value;
+        curr = successor;
+        parent = successorParent;
+    }
+
+    BTreeNode* child = (curr->left) ? curr->left : curr->right;
+
+    if (!parent)
+        root = child;
+    else if (parent->left == curr)
+        parent->left = child;
+    else
+        parent->right = child;
+
+    delete curr;
 }
 
 void BinaryTree::inorderPrint() {
-    inorder(root);
+    std::stack<BTreeNode*> st;
+    BTreeNode* curr = root;
+
+    while (curr || !st.empty()) {
+
+        while (curr) {
+            st.push(curr);
+            curr = curr->left;
+        }
+
+        curr = st.top();
+        st.pop();
+        std::cout << curr->value << ' ';
+
+        curr = curr->right;
+    }
+}
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ifstream fin("input.txt");
+    if (!fin) {
+        cerr << "Error opening input file.\n";
+        return 1;
+    }
+
+    BinaryTree tree;
+    int a, b;
+    while (fin >> a >> b) {
+        if (a == 1) {
+            tree.insert(b);
+        } else if (a == 2) {
+            cout << (tree.search(b) ? 1 : 0) << " ";
+        } else if (a == 3) {
+            tree.deleteValue(b);
+        }
+    }
+
+    cout << "\n";
+    tree.inorderPrint();
+    cout << "\n";
+
+    return 0;
 }
